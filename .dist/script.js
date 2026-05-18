ae791e5a27e5f120426c25c0dcbdddd0df3e8913
Niko Faeldin <bawah.niko11@gmail.com>
2026-05-18 19:35:56 +0800

    updated file

---
+++ .dist/script.js
@@ -0,0 +1,7 @@
+// State Variables
+let currentWater = 0;
+let dailyGoal = 0;
+
+// Select Elements
+const goalInput = document.getElementById("goalInput");
+const setGoalBtn = document.getElementById("setGoalBtn");
