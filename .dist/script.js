5f016e68f803795b612777eefb7b8b7ac1a8d425
Niko Faeldin <bawah.niko11@gmail.com>
2026-05-18 20:33:15 +0800

    updated file

--- .dist/script.js
+++ .dist/script.js
@@ -5,3 +5,15 @@ let dailyGoal = 0;
 // Select Elements
 const goalInput = document.getElementById("goalInput");
 const setGoalBtn = document.getElementById("setGoalBtn");
+
+const add250 = document.getElementById("add250");
+const add500 = document.getElementById("add500");
+
+const progressText = document.getElementById("progressText");
+const statusText = document.getElementById("status");
+const message = document.getElementById("message");
+
+const progressCircle = document.querySelector(".progress-circle");
+
+const resetBtn = document.getElementById("resetBtn");
+
